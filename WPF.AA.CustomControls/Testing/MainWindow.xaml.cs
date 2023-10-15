73735e24d2f25211c577f34a803b46fe1cb28f00
Aaron Amberman <aaronamberman@gmail.com>
2023-10-15 13:26:13 -0400

    Added updated testing. Minor fix for watermark foreground color for ShowHidePasswordBox.

--- WPF.AA.CustomControls/Testing/MainWindow.xaml.cs
+++ WPF.AA.CustomControls/Testing/MainWindow.xaml.cs
@@ -4,9 +4,21 @@ namespace Testing
 {
     public partial class MainWindow : Window
     {
+        private MainWindowViewModel viewModel;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            viewModel = new MainWindowViewModel
+            {
+                Value = 81
+            };
+
+            DataContext = viewModel;
         }
     }
 }
---
+++ WPF.AA.CustomControls/Testing/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Testing
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); ;
+        }
+
+        private int value;
+        public int Value
+        {
+            get { return value; }
+            set { this.value = value; OnPropertyChanged(); }
+        }
+    }
+}
