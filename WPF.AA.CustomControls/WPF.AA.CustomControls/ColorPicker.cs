a8a6fa5c16aed0e5063442a17b0e53c2140edd3a
Aaron Amberman <aaronamberman@gmail.com>
2024-02-24 15:35:26 -0500

    Basic visual color slider done. Need to make custom UI for it and make it work in terms of value assignment.

--- WPF.AA.CustomControls/WPF.AA.CustomControls/ColorPicker.cs
+++ WPF.AA.CustomControls/WPF.AA.CustomControls/ColorPicker.cs
@@ -3,9 +3,14 @@ using System.Windows.Controls;
 
 namespace WPF.AA.CustomControls
 {
-    /// <summary>A simple color picker control for WPF.</summary>
-    public class ColorPicker : UserControl
+    /// <summary>A simple RBG color picker control for WPF.</summary>
+    public class ColorPicker : Control
     {
+        // https://stackoverflow.com/questions/32513387/how-to-create-a-color-canvas-for-color-picker-wpf
+        // https://www.codeproject.com/Articles/36802/WPF-Colour-Slider
+            // https://www.codeproject.com/script/Articles/ViewDownloads.aspx?aid=36802
+        // https://bootcamp.uxdesign.cc/definitive-guide-to-wpf-colors-color-spaces-color-pickers-and-creating-your-own-colors-for-mere-f480935c6e94
+
         #region Properties
 
         /// <summary>Gets or sets the corner radius for the button.</summary>
