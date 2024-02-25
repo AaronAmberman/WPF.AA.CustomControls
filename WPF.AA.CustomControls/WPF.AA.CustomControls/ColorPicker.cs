55cd53af8402d4f9bb33b249f0681e1abdc8b894
Aaron Amberman <aaronamberman@gmail.com>
2024-02-24 20:20:09 -0500

    Basic ColorPicker look complete

--- WPF.AA.CustomControls/WPF.AA.CustomControls/ColorPicker.cs
+++ WPF.AA.CustomControls/WPF.AA.CustomControls/ColorPicker.cs
@@ -1,9 +1,11 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace WPF.AA.CustomControls
 {
     /// <summary>A simple RBG color picker control for WPF.</summary>
+    [TemplatePart(Name = "PART_ColorSlider", Type = typeof(Slider))]
     public class ColorPicker : Control
     {
         // https://stackoverflow.com/questions/32513387/how-to-create-a-color-canvas-for-color-picker-wpf
@@ -11,7 +13,23 @@ namespace WPF.AA.CustomControls
             // https://www.codeproject.com/script/Articles/ViewDownloads.aspx?aid=36802
         // https://bootcamp.uxdesign.cc/definitive-guide-to-wpf-colors-color-spaces-color-pickers-and-creating-your-own-colors-for-mere-f480935c6e94
 
+        #region Fields
+
+        private Slider colorSlider;
+
+        #endregion
+
         #region Properties
+
+        /// <summary>Gets or sets the base color (the color for the vertical color slider).</summary>
+        public Color BaseColor
+        {
+            get { return (Color)GetValue(BaseColorProperty); }
+            set { SetValue(BaseColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty BaseColorProperty =
+            DependencyProperty.Register("BaseColor", typeof(Color), typeof(ColorPicker), new PropertyMetadata(Colors.Red));
 
         /// <summary>Gets or sets the corner radius for the button.</summary>
         public CornerRadius CornerRadius
@@ -23,6 +41,17 @@ namespace WPF.AA.CustomControls
         public static readonly DependencyProperty CornerRadiusProperty =
             DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(ColorPicker), new PropertyMetadata(null));
 
+
+        /// <summary>Gets or sets the selected color (the color for the gradient color picker).</summary>
+        public Color SelectedColor
+        {
+            get { return (Color)GetValue(SelectedColorProperty); }
+            set { SetValue(SelectedColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedColorProperty =
+            DependencyProperty.Register("SelectedColor", typeof(Color), typeof(ColorPicker), new PropertyMetadata(Colors.Red));
+
         #endregion
 
         #region Constructors
@@ -30,6 +59,28 @@ namespace WPF.AA.CustomControls
         static ColorPicker()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorPicker), new FrameworkPropertyMetadata(typeof(ColorPicker)));
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void CacheVerticalColorBarGradient()
+        {
+
+        }
+
+        public override void OnApplyTemplate()
+        {
+            colorSlider = GetTemplateChild("PART_ColorSlider") as Slider;
+            colorSlider.ValueChanged += ColorSlider_ValueChanged;
+
+            base.OnApplyTemplate();
+        }
+
+        private void ColorSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            
         }
 
         #endregion
